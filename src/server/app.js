54bcc40493419da2ce22c9cd005670e7692b13f1
Rlawnfka <gimjulam2@gmail.com>
2025-08-20 15:15:19 +0900

    add edit image

---
+++ src/server/app.js
@@ -0,0 +1,13 @@
+const express = require("express");
+
+const app = express();
+const db = mysql.createConnection({
+    host : "localhost",
+    user : "root",
+    database : "quizdb"
+});
+
+db.connect(err => {
+    if(err) throw err;
+    console.log("MySQL 연결 성공!");
+});
