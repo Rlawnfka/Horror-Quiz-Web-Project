208484e1888e746785f17d42e36bdefdfb31621c
Rlawnfka <gimjulam2@gmail.com>
2025-09-09 16:31:13 +0900

    edit app.js

--- src/server/app.js
+++ src/server/app.js
@@ -1,11 +1,12 @@
 const express = require("express");
-const mysql = require("mysql");
 const app = express();
+const mysql = require('mysql');
+
 
 const db = mysql.createConnection({
     host : "localhost",
     user : "root",
-    password:"",
+    password:"0812",
     database : "find_the_answer"
 });
 
